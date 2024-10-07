ffba677788c0474029a247371368c93c2e539984
priyanka-ns <priyanka.singh@ripplehire.com>
2024-10-07 23:53:56 +0530

    Day 29

--- sort/MissingNumber.java
+++ sort/MissingNumber.java
@@ -43,6 +43,7 @@ public class MissingNumber {
         return n;
     }
 
+    //method to swap the numbers
     private static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
